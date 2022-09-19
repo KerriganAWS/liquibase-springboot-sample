ffd96bb70aac4710a4422fbf98e93b9a9669f6f2
Lin <yenhlin@88665a0e13fc.ant.amazon.com>
2022-09-20 01:09:39 +0800

    added k8s job yaml

--- src/main/java/com/demo/liquibase/LiquibaseDemo.java
+++ src/main/java/com/demo/liquibase/LiquibaseDemo.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LiquibaseDemo {
 
-    public static void main(String[] args) {
-        SpringApplication.run(LiquibaseDemo.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(LiquibaseDemo.class, args);
+  }
 }
